5f05b5e5d98fcc012a8bfe84f7c00d50e99d5479
villiam damstrom <villiam@live.se>
2024-05-12 17:57:55 +0200

    changed some java for the tutorialscreen and css

--- src/Components/tutorialScreen/tutorialScreen.jsx
+++ src/Components/tutorialScreen/tutorialScreen.jsx
@@ -1,15 +1,27 @@
 const TutorialScreen = () => {
     return (
        <div className="container-tutorial">
-        <p>Aliens attacked the moon! Whack the aliens with the hammer and earn points.
-        It's getting harder with time. Do NOT whack the bombs.
-        </p>
-        <img></img>
-        
-       </div>
-        
-    )
+        <div className="tutorial-header">
+        <h2>Aliens attacked the moon!</h2>
+        </div>
+        <div className="tutorial-text-first">
+            <p>Whack the aliens with the hammer and earn points.
+        It's getting harder with time. Do NOT whack the bombs.</p>
+        </div>
+        <div className="tutorial-icons">
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+        </div>
+        <div className="tutorial-text-second">
+            <p>Do NOT whack the bombs
+            You only have three lives.
+            Reach a 500 score to WIN!</p>
+        </div>
+        <div className="tutorial-alien">
+            <img src="" alt="" />
+        </div>
+       </div>     
+    )};
 
-
-}
 export default TutorialScreen 
